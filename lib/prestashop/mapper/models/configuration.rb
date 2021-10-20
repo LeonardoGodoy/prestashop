9c0c7a2b46dd823f019e6a0ec42bc5df91cca3bc
Leonardo Godoy <leonardo.godoy.souza@gmail.com>
2021-10-20 18:29:30 -0300

    Add configuration model

---
+++ lib/prestashop/mapper/models/configuration.rb
@@ -0,0 +1,9 @@
+using Prestashop::Mapper::Refinement
+module Prestashop
+  module Mapper
+    class Configuration < Model
+      resource :configurations
+      model :configuration
+    end
+  end
+end
