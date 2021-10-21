dc0038757e34d252c69f6746d486c80b36c3a4a3
Leonardo Godoy <leonardo.godoy.souza@gmail.com>
2021-10-21 17:28:41 -0300

    Remove useless error class

--- lib/prestashop/api_client.rb
+++ lib/prestashop/api_client.rb
@@ -1,13 +1,13 @@
-require 'prestashop/client/error'
 require 'prestashop/client/cache'
 
 module Prestashop
   class ApiClient
-    delegate  :create, :read, :update, :delete, :check, :upload, to: :connection
+    extend Forwardable
 
-    delegate  :manufacturers_cache, :clear_manufacturers_cache, :categories_cache, :clear_categories_cache, :features_cache, :clear_features_cache,
-              :feature_values_cache, :clear_feature_values_cache, :options_cache, :clear_options_cache, :option_values_cache, :clear_option_values_cache,
-              to: :cache
+    def_delegators :connection, :create, :read, :update, :delete, :check, :upload
+
+    def_delegators :cache, :manufacturers_cache, :clear_manufacturers_cache, :categories_cache, :clear_categories_cache, :features_cache, :clear_features_cache,
+                   :feature_values_cache, :clear_feature_values_cache, :options_cache, :clear_options_cache, :option_values_cache, :clear_option_values_cache
 
     attr_reader :api_key, :api_url, :connection, :cache
 
--- lib/prestashop/client/error.rb
+++
@@ -1,9 +0,0 @@
-module Prestashop
-  module Client
-    class UnitializedClient < RuntimeError
-      def initialize
-        super "Client isn't initialized"
-      end
-    end
-  end
-end
